4cdedab9b7941ff80e2b96954c64a9001125ae03
Selvin Selbaraju <selvin@selvinselbaraju-macbook-pro.home>
2022-04-18 11:14:03 +0100

    changed shebang to user's local shell

---
+++ hooks/gen_spec.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# This script generates the spec.yaml used in the API Gateway config.
+# Run this every time the server API changes.
+# Usage (from repo root): `sh scripts/generate_spec.sh`
+# Requires api-spec-converter: `npm install -g api-spec-converter)`
+
+set -euo pipefail
+
+poetry run python -c \
+    "from main import app; print(app.openapi())" \
+    > spec.json
+
+api-spec-converter \
+    --from=openapi_3 \
+    --to=swagger_2 \
+    --syntax=yaml \
+    spec.json \
+    > deploy/service/spec.yaml
+
+rm spec.json
