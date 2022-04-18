3aaffadd189172d31f6b39863717740b958626b5
Selvin Selbaraju <selvin@selvinselbaraju-macbook-pro.home>
2022-04-18 11:52:51 +0100

    removed git merge conflict tags

--- hooks/gen_spec.sh
+++ hooks/gen_spec.sh
@@ -4,23 +4,6 @@
 # Usage (from repo root): `sh scripts/generate_spec.sh`
 # Requires api-spec-converter: `npm install -g api-spec-converter)`
 
-<<<<<<< HEAD
-SRC_PATTERN="*.py"
-if git diff --cached --name-only | grep --quiet "$SRC_PATTERN"
-then
-    exit 0
-fi
-    set -euo pipefail
-        poetry run python -c \
-        "from main import app; print(app.openapi())" \
-        > spec.json \ &&
-        api-spec-converter \
-            --from=openapi_3 \
-            --to=swagger_2 \
-            --syntax=yaml \
-            spec.json \
-            > deploy/service/spec.yaml
-=======
 set -euo pipefail
 
 poetry run python -c \
@@ -35,4 +18,3 @@ api-spec-converter \
     > deploy/service/spec.yaml \
 && 
 rm spec.json
->>>>>>> parent of a74ba6f (removed remove spec step in script)
