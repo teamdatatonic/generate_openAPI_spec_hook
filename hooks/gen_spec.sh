75c6a4f00893a233bf1014b6a23b0ceb0edc1626
Selvin Selbaraju <selvin@selvinselbaraju-macbook-pro.home>
2022-04-18 11:50:10 +0100

    removed gif diff logic from hook

--- hooks/gen_spec.sh
+++ hooks/gen_spec.sh
@@ -4,6 +4,7 @@
 # Usage (from repo root): `sh scripts/generate_spec.sh`
 # Requires api-spec-converter: `npm install -g api-spec-converter)`
 
+<<<<<<< HEAD
 SRC_PATTERN="*.py"
 if git diff --cached --name-only | grep --quiet "$SRC_PATTERN"
 then
@@ -19,3 +20,19 @@ fi
             --syntax=yaml \
             spec.json \
             > deploy/service/spec.yaml
+=======
+set -euo pipefail
+
+poetry run python -c \
+    "from main import app; print(app.openapi())" \
+    > spec.json \
+&&
+api-spec-converter \
+    --from=openapi_3 \
+    --to=swagger_2 \
+    --syntax=yaml \
+    spec.json \
+    > deploy/service/spec.yaml \
+&& 
+rm spec.json
+>>>>>>> parent of a74ba6f (removed remove spec step in script)
