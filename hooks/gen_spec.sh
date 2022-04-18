a28f15d6c8b7f9766bf683bd9fe9e415f0ecdeb6
Selvin Selbaraju <selvin@selvinselbaraju-macbook-pro.home>
2022-04-18 11:42:24 +0100

    only run hook if .py files have changed

--- hooks/gen_spec.sh
+++ hooks/gen_spec.sh
@@ -4,15 +4,18 @@
 # Usage (from repo root): `sh scripts/generate_spec.sh`
 # Requires api-spec-converter: `npm install -g api-spec-converter)`
 
-set -euo pipefail
-
-poetry run python -c \
-    "from main import app; print(app.openapi())" \
-    > spec.json \
-&&
-api-spec-converter \
-    --from=openapi_3 \
-    --to=swagger_2 \
-    --syntax=yaml \
-    spec.json \
-    > deploy/service/spec.yaml \
+SRC_PATTERN="*.py"
+if git d iff --cached --name-only | grep --quiet "$SRC_PATTERN"
+then
+    exit 0
+fi
+    set -euo pipefail
+        poetry run python -c \
+        "from main import app; print(app.openapi())" \
+        > spec.json \ &&
+        api-spec-converter \
+            --from=openapi_3 \
+            --to=swagger_2 \
+            --syntax=yaml \
+            spec.json \
+            > deploy/service/spec.yaml
